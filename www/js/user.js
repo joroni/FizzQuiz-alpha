c5a1e5266321a9975d0c0a124c31c229ede454d4
kophonegap <niconi@coca-cola.com>
2016-08-31 02:27:31 +0800

    updated

--- www/js/user.js
+++ www/js/user.js
@@ -9,15 +9,24 @@ $(function() {
 
     $.getJSON("http://104.238.96.209/~project/db/get_user_details/"+user,function(result){
       $.each(result, function(i, field){
-        $("#output").append("<tr><td>Username:  "+ field.username + " </td></tr><tr><td>Password: "+ field.password + "</td></tr><tr><td>Email: "+ field.email + "</td></tr>");
-		$('#user_username').val(field.username);
-		$('#user_password').val(field.password);
-		  $('#user_email').val(field.email);
-		$('#fname').val(field.fname);
-		$('#user_firstname').text(field.fname);
-		  $('#email').val(field.email);
+       // $("#output").append("<tr><td>Username:  "+ field.username + " </td></tr><tr><td>Password: "+ field.password + "</td></tr><tr><td>Email: "+ field.email + "</td></tr>");
+		//$('#user_username').val(field.username);
+		//$('#user_password').val(field.password);
+		//  $('#user_email').val(field.email);
+		//$('#user_firstname').val(field.fname);
+	//	$('#user_lastname').text(field.lname);
 
-		  $('#user_email').text(field.email);
+         /* $("#output").append("<table><tr><td>Username:  "+ field.username + "</td></tr>" +
+              "<tr><td>Password: "+ field.password + "</td></tr>" +
+              "<tr><td>Email: "+ field.email + "</td></tr>" +
+              "</table>");*/
+
+
+          $('#user_username').val(field.username);
+          $('#user_password').val(field.password);
+          $('#user_firstname').val(field.fname);
+          $('#user_lastname').val(field.lname);
+          $('#user_email').val(field.email);
      });
     });
  });
@@ -25,17 +34,26 @@ $(function() {
 
 function update_user(){
 	var username = $('#user_username').val();
- 	var fname = $('#fname').val();
-	var email = $('#email').val();
+    var fname = $('#fname').val();
+ 	var lname = $('#lname').val();
+	//var email = $('#email').val();
 
-$.post( "http://104.238.96.209/~project/db/update/user", { username: username, fname: fname })
-  .done(function( data ) {
+$.post( "http://104.238.96.209/~project/db/update/user", { username: username, fname: fname, lname: lname, email: email})
+//    $.post( "http://104.238.96.209/~project/db/update/user", { username: username, fname: fname})
+.done(function( data ) {
 		if(data == 0){
 	 		$('#update_0').show();
 		}else if(data == 1){
 			$('#update_1').show();
 			$('#user_firstname').text(fname);
-			$('#user_email').text(email);
+			$('#user_lastname').text(lname);
+            $('#user_email').text(email);
+
+            $('#user_username').val(field.username);
+           // $('#user_password').val(field.password);
+            $('#user_firstname').val(field.fname);
+            $('#user_lastname').val(field.lname);
+            $('#user_email').val(field.email);
 			//window.location.href = "user.html";
 		}
   });
